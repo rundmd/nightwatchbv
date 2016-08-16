9625e4e341e0cb56fe9b444f041381f9d342af8b
rundmd <drew.dimanlig@gmail.com>
2016-08-16 10:49:12 -0500

    remove old test directory

--- tests/support/pages/consoleElementsPage.js
+++
@@ -1,7 +0,0 @@
-define({
-  SUBMISSION_URL:'https://scissors.feedmagnet.com/input/group/47/moderation/inbox/',
-  USERNAME_LOCATOR: '//*[@id="id_username"]',
-  PASSWORD_LOCATOR: '//*[@id="id_password"]',
-  SUBMIT_BTN_LOCATOR: '//*[@id="fm-form-login"]/div/button'
-});
-
