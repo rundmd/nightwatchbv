aa02be5f0662e97c958408a289fc8dccd96dddd4
rundmd <drew.dimanlig@gmail.com>
2016-11-22 00:56:52 -0600

    Add a pause so the test waits until the rejection/approval is complete. Can change to a waitUntil when aperture is updated.

--- nightwatch/pages/moderatePostPage.js
+++ nightwatch/pages/moderatePostPage.js
@@ -2,24 +2,26 @@ var locators = require('../config/locators.js');
 var properties = require('../config/properties.js');
 
 var moderateCommands = {
-  approve: function() {
-    return this.waitForElementVisible(locators.MODERATE_GROUP_FILTER_LOCATOR, properties.DEFAULT_TIMEOUT)
+  approve: function(client) {
+    return client.waitForElementVisible(locators.MODERATE_GROUP_FILTER_LOCATOR, properties.DEFAULT_TIMEOUT)
       .click(locators.MODERATE_GROUP_FILTER_LOCATOR)
       .click(locators.FILTER_OPTION_0_LOCATOR)
       .waitForElementVisible(locators.POST_LOCATOR, properties.DEFAULT_TIMEOUT)
       .click(locators.POST_LOCATOR)
       .waitForElementVisible(locators.APPROVE_ICON_LOCATOR, properties.DEFAULT_TIMEOUT)
-      .click(locators.APPROVE_ICON_LOCATOR);
+      .click(locators.APPROVE_ICON_LOCATOR)
+      .pause(5000);
   },
 
-  reject: function() {
-    return this.waitForElementVisible(locators.MODERATE_GROUP_FILTER_LOCATOR, properties.DEFAULT_TIMEOUT)
+  reject: function(client) {
+    return client.waitForElementVisible(locators.MODERATE_GROUP_FILTER_LOCATOR, properties.DEFAULT_TIMEOUT)
       .click(locators.MODERATE_GROUP_FILTER_LOCATOR)
       .click(locators.FILTER_OPTION_0_LOCATOR)
       .waitForElementVisible(locators.POST_LOCATOR, properties.DEFAULT_TIMEOUT)
       .click(locators.POST_LOCATOR)
       .waitForElementVisible(locators.REJECT_ICON_LOCATOR, properties.DEFAULT_TIMEOUT)
-      .click(locators.REJECT_ICON_LOCATOR);
+      .click(locators.REJECT_ICON_LOCATOR)
+      .pause(5000);
   }
 
 };
